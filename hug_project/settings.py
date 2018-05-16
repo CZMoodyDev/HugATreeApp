1758152ed42a2917c9b549be51da892cc8744330
Curtis Moody <CZMoodyDev@gmail.com>
2018-05-15 20:46:56 -0700

    * Added a basic view as an example for future use

--- hug_project/settings.py
+++ hug_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hug_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'hug_project_app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
