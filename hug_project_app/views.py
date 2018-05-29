92763fbf35c97f0e173a8d1edd59cb37bf32dfa9
Alexis Jensen <jensen.alexis@gmail.com>
2018-05-28 20:42:42 -0700

    second attempt at getting mutliple html pages to run

--- hug_project_app/views.py
+++ hug_project_app/views.py
@@ -3,7 +3,6 @@ from django.template import loader
 
 def index(request):
     template = loader.get_template('index.html')
-
     context = { }
     return HttpResponse(template.render(context, request))
 
