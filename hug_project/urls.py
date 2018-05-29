42cec7dc6148b1a4f5d6b2fbc6c78ba2c1939677
Alexis Jensen <jensen.alexis@gmail.com>
2018-05-28 19:40:22 -0700

    second attempt at getting mutliple html pages to run

--- hug_project/urls.py
+++ hug_project/urls.py
@@ -10,19 +10,14 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 
-    
-***** Curtis I TRIED THIS ONE TOO:
-
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from hug_project_app import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('', views.about, name='about'),
+    path('hug_project_app/', include('hug_project_app.urls')),
 ]
