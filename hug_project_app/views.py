8a4dc50625fb8a4f2717c7ccbb2d47ddfc66e354
Alexis Jensen <jensen.alexis@gmail.com>
2018-06-21 21:48:29 -0700

    built up tree_detail view and html so it is being fed tree info from the database - right now just using tree #12 until link from index page gives correct tree id parameter

--- hug_project_app/views.py
+++ hug_project_app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from hug_project_app.models import Tree
+from datetime import datetime
+import calendar
+
 
 def index(request):
     trees = []
-    
+
     tree_objects = Tree.objects.all()
-    
+
     for tree in tree_objects:
         single_tree = {}
-        
+
         single_tree["name"] = tree.COMMON_NAME
         single_tree["type"] = "Tree"
         single_tree["icon"] = "TODO"
@@ -19,16 +22,54 @@ def index(request):
         single_tree["diameter"] = tree.DIAMETER
         single_tree["details"] = "TODO"
         single_tree["TREE_ID"] = tree.TREE_ID
-        
+
         trees.append(single_tree)
 
     return render(request, 'index.html', {"trees": trees})
-    
+
 def about(request):
     return render(request, 'about.html')
 
 def tree_detail(request):
-    return render(request, 'tree_detail.html')
+    context_dict = {}
+    try:
+        # TODO: Curtis add TREE_ID parameter where 'id__exact=12' is
+        # (The tree_id is a parameter passed to this view via the index page when someone clicks on a tree link)
+        # This: tree = Tree.objects.get(id__exact=12) should be replaced with something like
+        # tree = Tree.objects.get(id=id)
+        tree = Tree.objects.get(id__exact=12)
+        context_dict['tree_id'] = tree.TREE_ID
+        context_dict['common_name'] = tree.COMMON_NAME
+        context_dict['diameter'] = tree.DIAMETER
+
+        # get and break date apart
+        date = tree.DATE_PLANTED
+        year = int(str(date)[:4])
+        month = int(str(date)[4:6])
+        day = int(str(date)[6:8])
+
+        # get current date to find age of tree
+        today = datetime.today()
+        current_year = today.year
+        age = current_year - year
+
+        # get name of the month eg: December rather than 12
+        month_name = calendar.month_name[month]
+
+        # birthdate eg: December 12, 1979 (39 years old)
+        context_dict['birthdate'] = month_name + ' ' + str(day) + ', ' + str(year) + ' (' + str(age) + ' years old)'
+
+        # address eg: 2799 W 23rd Av
+        address_number = tree.CIVIC_NUMBER
+        street = tree.STD_STREET
+        context_dict['address'] = str(address_number) + ' ' + street
+
+        context_dict['neighbourhood'] = tree.NEIGHBOURHOOD_NAME
+
+    except Tree.DoesNotExist:
+        pass
+    return render(request, 'tree_detail.html', context_dict)
+
 
 def favourites(request):
     return render(request, 'favourites.html')
