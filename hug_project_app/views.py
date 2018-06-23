f8b48de04c7e3945cb58a86b241c4c2d9a549cfe
Alexis Jensen <jensen.alexis@gmail.com>
2018-06-22 21:39:20 -0700

    Worked on tree_detail views so it can get data from all three types: tree, park and garden. Then updated tree_detail.html so that it will render properly based on whatever object it receives.
    **tree_detail view is not complete.  Need to figure out how to handle problem of passing one object where 3 objects are being defined.

--- hug_project_app/views.py
+++ hug_project_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from hug_project_app.models import Tree
+from hug_project_app.models import Tree, FoodTree, Park, User, UserProfile
 from datetime import datetime
 import calendar
 
@@ -33,38 +33,62 @@ def about(request):
 def tree_detail(request):
     context_dict = {}
     try:
-        # TODO: Curtis add TREE_ID parameter where 'id__exact=12' is
-        # (The tree_id is a parameter passed to this view via the index page when someone clicks on a tree link)
-        # This: tree = Tree.objects.get(id__exact=12) should be replaced with something like
-        # tree = Tree.objects.get(id=id)
-        tree = Tree.objects.get(id__exact=12)
-        context_dict['tree_id'] = tree.TREE_ID
-        context_dict['common_name'] = tree.COMMON_NAME
-        context_dict['diameter'] = tree.DIAMETER
+        # TODO: Curtis add TREE_ID or Park_ID or FoodTree_ID parameter
+        # How to handle try and excepts with three objects?
 
-        # get and break date apart
-        date = tree.DATE_PLANTED
-        year = int(str(date)[:4])
-        month = int(str(date)[4:6])
-        day = int(str(date)[6:8])
+        # ********* Tree Info **********************************************
+        # tree = Tree.objects.get(id__exact=12)
+        # context_dict['tree_id'] = tree.TREE_ID
+        # context_dict['common_name'] = tree.COMMON_NAME
+        # context_dict['diameter'] = tree.DIAMETER
+        #
+        # # get and break date apart
+        # date = tree.DATE_PLANTED
+        # year = int(str(date)[:4])
+        # month = int(str(date)[4:6])
+        # day = int(str(date)[6:8])
+        #
+        # # get current date to find age of tree
+        # today = datetime.today()
+        # current_year = today.year
+        # age = current_year - year
+        #
+        # # get name of the month eg: December rather than 12
+        # month_name = calendar.month_name[month]
+        #
+        # # birthdate eg: December 12, 1979 (39 years old)
+        # context_dict['birthdate'] = month_name + ' ' + str(day) + ', ' + str(year) + ' (' + str(age) + ' years old)'
+        #
+        # # address eg: 2799 W 23rd Av
+        # address_number = tree.CIVIC_NUMBER
+        # street = tree.STD_STREET
+        # context_dict['address'] = str(address_number) + ' ' + street
+        #
+        # context_dict['neighbourhood'] = tree.NEIGHBOURHOOD_NAME
 
-        # get current date to find age of tree
-        today = datetime.today()
-        current_year = today.year
-        age = current_year - year
+        # ********* FoodTree Info **********************************************
+        # food = FoodTree.objects.get(id__exact=2)
+        # context_dict['food_id'] = food.MAPID
+        # context_dict['garden_name'] = food.NAME
+        # context_dict['year_created'] = food.YEAR_CREATED
+        # context_dict['number_of_trees'] = food.NUMBER_OF_FOOD_TREES
+        # context_dict['food_varieties'] = food.FOOD_TREE_VARIETIES
+        # context_dict['garden_address'] = food.MERGED_ADDRESS
 
-        # get name of the month eg: December rather than 12
-        month_name = calendar.month_name[month]
+        # Need to create neighbourhood field to Community
+        # Garden data set and add data manually
+        # context_dict['garden_neighbourhood'] = food.Neighbourhood
 
-        # birthdate eg: December 12, 1979 (39 years old)
-        context_dict['birthdate'] = month_name + ' ' + str(day) + ', ' + str(year) + ' (' + str(age) + ' years old)'
-
-        # address eg: 2799 W 23rd Av
-        address_number = tree.CIVIC_NUMBER
-        street = tree.STD_STREET
-        context_dict['address'] = str(address_number) + ' ' + street
-
-        context_dict['neighbourhood'] = tree.NEIGHBOURHOOD_NAME
+        # ********* Park Info **********************************************
+        park = Park.objects.get(id__exact=3)
+        context_dict['park_id'] = park.ParkID
+        context_dict['park_name'] = park.Name
+        # .normalize() strips trailing zeroes off 2.8900 becomes 2.89
+        context_dict['park_size'] = park.Hectare.normalize()
+        park_address_number = park.StreetNumber
+        park_street = park.StreetName
+        context_dict['park_address'] = str(park_address_number) + ' ' + park_street
+        context_dict['park_neighbourhood'] = park.NeighbourhoodName
 
     except Tree.DoesNotExist:
         pass
